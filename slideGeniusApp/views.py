344701f031c1d3af9ea3dcbbbf1962dc55a8aaf0
Agnes <agypeter97@gmail.com>
2024-05-16 00:46:52 +0300

    function to handle next/prev button functionalities

--- slideGeniusApp/views.py
+++ slideGeniusApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import openai
+from django.shortcuts import redirect
 
 def index(request):
     if request.method == 'POST':
@@ -25,3 +26,22 @@ def index(request):
         })
 
     return render(request, 'index.html')
+
+
+
+def next_slide(request):
+    if request.method == 'POST':
+        current_slide = int(request.POST.get('current_slide', 1))
+        num_slides = int(request.POST.get('num_slides', 1))
+
+        next_slide = current_slide + 1 if current_slide < num_slides else num_slides
+        return redirect('index')
+    return redirect('index')  
+
+def previous_slide(request):
+    if request.method == 'POST':
+        current_slide = int(request.POST.get('current_slide', 1))
+
+        previous_slide = current_slide - 1 if current_slide > 1 else 1
+        return redirect('index')
+    return redirect('index')

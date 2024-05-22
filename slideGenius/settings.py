1faee068433866bff54022aa841bd65ff0b44c28
Agnes <agypeter97@gmail.com>
2024-05-22 20:24:38 +0300

    prod files

--- slideGenius/settings.py
+++ slideGenius/settings.py
@@ -79,17 +79,18 @@ WSGI_APPLICATION = 'slideGenius.wsgi.application'
 
 
 # Database
-# Comment out or remove the following DATABASES configuration
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.getenv('DB_NAME'),
-#         'USER': os.getenv('DB_USER'),
-#         'PASSWORD': os.getenv('DB_PASSWORD'),
-#         'HOST': os.getenv('DB_HOST'),
-#         'PORT': os.getenv('DB_PORT')
-#     }
-# }
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
+    }
+}
 
 
 # Password validation

41b2abd7fba9b0447987fd7ca7f3269841712c6d
Agnes <agypeter97@gmail.com>
2024-05-28 19:40:06 +0300

    refactored

--- slideGenius/settings.py
+++ slideGenius/settings.py
@@ -60,8 +60,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:8000",
-    "http://localhost:8000",
+    # "http://127.0.0.1:8000",
+    # "http://localhost:8000",
     "https://slide-genius-5.onrender.com",
 ]
 
